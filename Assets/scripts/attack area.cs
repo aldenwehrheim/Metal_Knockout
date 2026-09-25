3d2cec5d24ce0f444ee2b57f44c8c193b2b9a30c
AldenWehrheim <wehrheima@rsd407.org>
2026-09-25 09:07:35 -0700

    started adding first attack

--- Assets/scripts/attack area.cs
+++ Assets/scripts/attack area.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 public class attackarea : MonoBehaviour
 {
     private float angle;
-    
+    private int damage = 1;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-     if (!Input.GetKeyDown(KeyCode.Space))
-     {
+        if(collider.GetComponent<health>() != null)
+        {
+            health Health = collider.
+        
+
         Rigidbody2D body = collider.GetComponent<Rigidbody2D>();
         Transform angle = collider.GetComponent<Transform>();
         
         if (body != null)
-    {   
+     
         body.AddTorque(Random.Range(-150f, 150f));
         body.linearVelocity = new Vector2(30f, 10f);
-        
-    }   
-     }
-    }   
+        }
+    }
 }       
--- Assets/scripts/playerattack.cs
+++ Assets/scripts/playerattack.cs
@@ -1,6 +1,39 @@
+using Unity.Collections.Tests.CoreCLR.TestJobs;
 using UnityEngine;
 
-public class playerattack
+public class playerattack : MonoBehaviour
 {
-    
-}
+   private GameObject attackarea = default;
+   private bool attacking = false;
+   private float timeToAttack = 0.25f;
+   private float timer = 0f;
+
+    void Start()
+    {
+        attackarea = transform.GetChild(0).gameObject;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            attack();
+        }
+        if (attacking)
+        {
+            timer += Time.deltaTime;
+
+            if(timer >= timeToAttack)
+            {
+                timer = 0;
+                attacking = false;
+                attackarea.SetActive(attacking);
+            }
+        }
+    }
+    private void attack()
+    {
+        attacking = true;
+        attackarea.SetActive(attacking);
+    }
+}       
